ca52781beb359b4a352fab5f62994fe92ce65a3a
solarboyh <841796739@qq.com>
2021-09-25 16:27:43 +0800

    third1 commit

--- src/main/java/com/xyn/git/GitTest.java
+++ src/main/java/com/xyn/git/GitTest.java
@@ -3,6 +3,7 @@ package com.xyn.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");System.out.println("hello world!");
         System.out.println("hello world!");
     }
 }
